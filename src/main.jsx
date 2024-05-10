7bf0d7ae08973816acc8cacb0138fe47acfc1290
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2024-05-10 14:16:50 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import "modern-normalize";
 
 import { Provider } from 'react-redux';
 
-import { store } from "../src/redux/store";
+import { persistor, store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from '../src/components/App/App';
 
@@ -16,7 +17,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <Provider store={store}>
+     <PersistGate persistor={persistor}>
     <App />
+    </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
